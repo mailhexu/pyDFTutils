bb6cb01f3ebb3dee9599a1da04a3c4567f10199b
hexu <mailhexu@gmail.com>
2022-08-27 12:32:49 +0200

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyDFTutils',
-    version='0.1.6',
+    version='0.1.7',
     description='utils for DFT/TB calculation',
     author='Xu He',
     author_email='mailhexu@gmail.com',
