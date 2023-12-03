51e9aacbb8f8d81ea5072c7bf695eae9496d59ae
hexu <mailhexu@gmail.com>
2023-12-03 22:30:08 +0100

    add argparse to view_pos_info and gen_primitive

--- scripts/gen_primitive.py
+++ scripts/gen_primitive.py
@@ -3,11 +3,22 @@ from ase.io import read
 from ase.io import write
 from pyDFTutils.ase_utils.geometry import find_primitive
 import sys
-def gen(src_file ,des_file, symprec=1e-4):
+import argparse
+
+def gen(src_file ,des_file, symprec=1e-4, angle_tolerance=-1.0):
     atoms=read(src_file)
-    new_atoms=find_primitive(atoms, symprec=symprec, )
+    new_atoms=find_primitive(atoms, symprec=symprec, angle_tolerance=angle_tolerance )
     write(des_file, new_atoms)
     return atoms
 
 if __name__=='__main__':
-    gen(sys.argv[1],sys.argv[2])
+    #gen(sys.argv[1],sys.argv[2])
+    parser = argparse.ArgumentParser(description='generate primitive cell')
+    parser.add_argument('src_file', help='source file')
+    parser.add_argument('des_file', help='destination file')
+    parser.add_argument('-s', '--symprec', type=float, default=1e-4, help='symmetry precision')
+    parser.add_argument('-a', '--angle_tolerance', type=float, default=-1.0, help='angle tolerance')
+    args = parser.parse_args()
+    gen(args.src_file, args.des_file, args.symprec, args.angle_tolerance)
+    
+
--- scripts/view_pos_info.py
+++ scripts/view_pos_info.py
@@ -3,27 +3,64 @@ from ase.io import read
 from ase.geometry.cell import cell_to_cellpar
 import spglib.spglib as spglib
 import sys
+import argparse
 
-def view_cellpars(filename='POSCAR'):
-    atoms=read(filename)
-    print('CELLPARS: %s'%cell_to_cellpar(atoms.get_cell()))
-    print('Volume: %s'%atoms.get_volume())
 
-def view_spacegroup(filename='POSCAR',symprec=1e-4):
-    atoms=read(filename)
-    print("SPACEGROUP: %s"%spglib.get_spacegroup(atoms,symprec=symprec))
-    #print(spglib.get_symmetry_dataset(atoms, symprec=symprec))
+def view_cellpars(filename="POSCAR"):
+    atoms = read(filename)
+    print("CELLPARS: %s" % cell_to_cellpar(atoms.get_cell()))
+    print("Volume: %s" % atoms.get_volume())
 
-def viewall(filename='POSCAR',symprec=1e-4, angle_tolerance=-1.0):
-    view_cellpars(filename=filename)
-    view_spacegroup(filename=filename,symprec=symprec, angle_tolerance=angle_tolerance)
 
-if __name__=='__main__':
-    if len(sys.argv)==1:
-        view_spacegroup(filename='POSCAR')
-    elif len(sys.argv)==2:
-        view_spacegroup(filename=sys.argv[1])
-    elif len(sys.argv)==3:
-        view_spacegroup(filename=sys.argv[1],symprec=float(sys.argv[2]), angle_tolerance=)
+def view_spacegroup(
+    filename="POSCAR", symprec=1e-4, angle_tolerance=-1.0, view_all=False
+):
+    atoms = read(filename)
+    if not view_all:
+        print(
+            "SPACEGROUP: %s"
+            % spglib.get_spacegroup(
+                atoms, symprec=symprec, angle_tolerance=angle_tolerance
+            )
+        )
     else:
-        print("Error")
+        print(
+            spglib.get_symmetry_dataset(
+                atoms, symprec=symprec, angle_tolerance=angle_tolerance
+            )
+        )
+
+
+def viewall(filename="POSCAR", symprec=1e-4, angle_tolerance=-1.0):
+    view_cellpars(filename=filename)
+    view_spacegroup(filename=filename, symprec=symprec, angle_tolerance=angle_tolerance)
+
+
+if __name__ == "__main__":
+    # if len(sys.argv)==1:
+    #    view_spacegroup(filename='POSCAR')
+    # elif len(sys.argv)==2:
+    #    view_spacegroup(filename=sys.argv[1])
+    # elif len(sys.argv)==3:
+    #    view_spacegroup(filename=sys.argv[1],symprec=float(sys.argv[2]), angle_tolerance=)
+    # else:
+    #    print("Error")
+    parser = argparse.ArgumentParser(
+        description="View spacegroup and cellpars of atomic structure file"
+    )
+    parser.add_argument(
+        "filename", type=str, default="POSCAR", help="atomic structure file. Default: POSCAR"
+    )
+    parser.add_argument("-s", "--symprec", type=float, default=1e-4, help="symprec, default: 1e-4")
+    parser.add_argument(
+        "-a", "--angle_tolerance", type=float, default=-1.0, help="angle_tolerance, default: -1.0"
+    )
+    parser.add_argument("-v", "--view_all", action="store_true", help="view all")
+
+    args = parser.parse_args()
+    view_spacegroup(
+        filename=args.filename,
+        symprec=args.symprec,
+        angle_tolerance=args.angle_tolerance,
+        view_all=args.view_all,
+    )
