6beca24eb5984f166bdfa97987cd9670e9a3e198
mailhexu <mailhexu@gmail.com>
2025-11-21 15:01:03 +0100

    version-> 0.2.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyDFTutils",
-    version="0.2.8",
+    version="0.2.9",
     description="utils for DFT/TB calculation",
     author="Xu He",
     author_email="mailhexu@gmail.com",
